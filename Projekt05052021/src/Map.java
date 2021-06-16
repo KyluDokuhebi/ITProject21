33d99e08f7729029b927221d9da55fbbb22dde24
mickova <mickova@PC547.SPSEJECNANET.CZ>
2021-06-16 09:08:13 +0200

    16.6.2021

--- Projekt05052021/src/Map.java
+++ Projekt05052021/src/Map.java
@@ -1,10 +1,17 @@
-import java.io.IOException;
+import java.io.IOException
 import java.io.Serializable;
 import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Map extends HashMap<Integer, Local> implements Serializable{
+	
+private final String FILE_PATH = M:\IT\ITProject21\ITProject21\ITProject21\Projekt05052021\src\TheMap;	
+
+BufferedReader reader = new BufferedReader(FileReader(FILE_PATH));
+
+
+	
 private int start = 6;
 private int nowAt = start;
 
@@ -12,9 +19,7 @@ public String nowAt() {
 	return Local.toString();
 }
 
-public static Map load(String file) throws IOException{
-	return null;
-}
+
 
 
 public boolean goTo(int dir) {
